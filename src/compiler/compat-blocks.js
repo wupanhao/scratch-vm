23ff710a2ae73f4537527068cac620cbd66abd81
Thomas Weber <muffin@muffin.ink>
2021-12-25 20:50:50 -0600

    Add more legacy no-op blocks to compatibility layer

--- src/compiler/compat-blocks.js
+++ src/compiler/compat-blocks.js
@@ -25,16 +25,20 @@ const stacked = [
     'control_clear_counter',
     'control_incr_counter',
     'looks_changestretchby',
+    'looks_hideallsprites',
     'looks_say',
     'looks_sayforsecs',
     'looks_setstretchto',
     'looks_switchbackdroptoandwait',
     'looks_think',
     'looks_thinkforsecs',
+    'motion_align_scene',
     'motion_glidesecstoxy',
     'motion_glideto',
     'motion_goto',
     'motion_pointtowards',
+    'motion_scroll_right',
+    'motion_scroll_up',
     'sensing_askandwait',
     'sensing_setdragmode',
     'sound_changeeffectby',
@@ -49,8 +53,11 @@ const stacked = [
 
 const inputs = [
     'control_get_counter',
+    'motion_xscroll',
+    'motion_yscroll',
     'sensing_loud',
     'sensing_loudness',
+    'sensing_userid',
     'sound_volume'
 ];
 
