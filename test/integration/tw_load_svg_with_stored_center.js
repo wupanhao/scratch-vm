73c02614808a5bffc9c9f0d5807e0a65315b93fc
GarboMuffin <muffin@muffin.ink>
2022-08-02 00:06:43 -0500

    Test that the center given to the renderer is what's stored in the SVG

--- test/integration/tw_load_svg_with_stored_center.js
+++ test/integration/tw_load_svg_with_stored_center.js
@@ -7,10 +7,17 @@ const {test} = require('tap');
 /* global TextEncoder */
 
 test('importing SVG with stored rotation center', async t => {
+    t.plan(3);
     const runtime = new Runtime();
     const storage = makeTestStorage();
     runtime.attachStorage(storage);
     const renderer = new FakeRenderer();
+    renderer.createSVGSkin = (svgText, rotationCenter) => {
+        // Make sure that the rotation center given to the renderer is correct
+        t.same(rotationCenter, [106.62300344745225, -11.822572945859918]);
+        // just need to return a valid skin ID, doesn't matter
+        return 1;
+    };
     runtime.attachRenderer(renderer);
     const svg = new TextEncoder().encode(
         `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"></svg><!--rotationCenter:106.62300344745225:-11.822572945859918-->`
