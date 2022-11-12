688b257f7c9cb445cfe472458e2c657802c338e4
GarboMuffin <muffin@mailbox.org>
2022-11-11 23:33:43 -0600

    Add tests for addAddonBlock

---
+++ test/integration/tw_addon_blocks.js
@@ -0,0 +1,95 @@
+const tap = require('tap');
+const fs = require('fs');
+const path = require('path');
+const VirtualMachine = require('../../src/virtual-machine');
+
+const fixtureData = fs.readFileSync(path.join(__dirname, '..', 'fixtures', 'tw-addon-blocks.sb3'));
+
+const runTests = compilerEnabled => async test => {
+    const load = async () => {
+        const vm = new VirtualMachine();
+        vm.setCompilerOptions({
+            enabled: compilerEnabled
+        });
+        await vm.loadProject(fixtureData);
+        vm.greenFlag();
+        return vm;
+    };
+
+    const getOutput = vm => vm.runtime.getTargetForStage().lookupVariableByNameAndType('output').value;
+
+    await test.test('simple use', async t => {
+        t.plan(6);
+
+        const vm = await load();
+
+        let calledBlock1 = false;
+        let calledBlock2 = false;
+    
+        vm.addAddonBlock({
+            procedureCode: 'block 2 %s',
+            callback: (args, thread) => {
+                calledBlock1 = true;
+                t.type(thread.peekStack, 'function');
+                t.deepEqual(args, {
+                    'number or text': 'banana'
+                });
+            },
+            arguments: ['number or text'],
+            color: '#ff4c4c',
+            secondaryColor: '#4cffff'
+        });
+    
+        vm.addAddonBlock({
+            procedureCode: 'block 3',
+            // eslint-disable-next-line no-unused-vars
+            callback: (args, thread) => {
+                calledBlock2 = true;
+                t.deepEqual(args, {});
+            },
+            arguments: [],
+            color: '#ff4c4c',
+            secondaryColor: '#4cffff'
+        });
+    
+        vm.runtime._step();
+
+        t.equal(getOutput(vm), 'block 1 value');
+        t.ok(calledBlock1);
+        t.ok(calledBlock2);
+        t.end();
+    });
+
+    await test.test('yielding block', async t => {
+        const vm = await load();
+
+        let threadToResume;
+
+        vm.addAddonBlock({
+            procedureCode: 'block 1',
+            callback: (args, thread) => {
+                thread.status = 1; // STATUS_PROMISE_WAIT
+                threadToResume = thread;
+            },
+            arguments: ['number or text'],
+            color: '#ff4c4c',
+            secondaryColor: '#4cffff'
+        });
+
+        vm.runtime._step();
+        if (!threadToResume) {
+            t.fail('did not run addon block');
+        }
+        t.equal(getOutput(vm), 'initial value');
+        threadToResume.status = 0; // STATUS_RUNNING
+        vm.runtime._step();
+        t.equal(getOutput(vm), 'block 3 value');
+
+        t.end();
+    });
+
+    test.end();
+};
+
+tap.test('with compiler disabled', runTests(false));
+tap.todo('with compiler enabled', runTests(true));
