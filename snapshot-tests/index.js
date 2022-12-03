2e4e67b1f0e7aa3fe03aaa20f364a36689f07e6a
GarboMuffin <muffin@mailbox.org>
2022-12-03 02:12:55 -0600

    Implement primitive compiler output snapshot testing
    If the compiler's type analysis becomes smarter, being able to show that
    its output matches what is expected will become more important. Snapshot
    testing is how we will solve that.

---
+++ snapshot-tests/index.js
@@ -0,0 +1,100 @@
+const Snapshot = require('./snapshots.js');
+
+/* eslint-disable no-console */
+
+const RESET = `\u001b[0m`;
+const BOLD = '\u001b[1m';
+const RED = '\u001b[31m';
+const BLUE = `\u001b[34m`;
+const GREEN = '\u001b[32m';
+const GRAY = '\u001b[90m';
+
+const PASSED = 'PASSED';
+const FAILED = 'FAILED';
+const UPDATED = 'UPDATED';
+
+const isUpdatingSnapshots = process.argv.includes('--update');
+
+const runSnapshotTest = async test => {
+    try {
+        const generatedSnapshot = await Snapshot.generateActualSnapshot(test);
+        const expectedSnapshot = Snapshot.getExpectedSnapshot(test);
+        const matches = generatedSnapshot === expectedSnapshot;
+
+        const prefix = `### ${test}: `;
+
+        if (isUpdatingSnapshots) {
+            if (matches) {
+                console.log(`${BOLD}${GREEN}${prefix}already matches${RESET}`);
+                return PASSED;
+            }
+            console.log(`${BOLD}${BLUE}### ${test}: updating${RESET}`);
+            Snapshot.saveSnapshot(test, generatedSnapshot);
+            return UPDATED;
+        }
+
+        if (matches) {
+            console.log(`${BOLD}${GREEN}### ${test}: matches${RESET}`);
+            return PASSED;
+        }
+
+        if (expectedSnapshot === null) {
+            console.log(`${BOLD}${BLUE}### ${test}: missing data, saving generated snapshot${RESET}`);
+            Snapshot.saveSnapshot(test, generatedSnapshot);
+            return UPDATED;
+        }
+
+        console.log(`${BOLD}${RED}### ${test}: DOES NOT MATCH${RESET}`);
+        console.log(`${RED}EXPECTED:\n${expectedSnapshot}${RESET}`);
+        console.log(`${BLUE}GOT:\n${generatedSnapshot}${RESET}`);
+    } catch (e) {
+        console.log(`${BOLD}${RED}### ${test}: ERROR${RESET}`);
+        console.log(`${RED}${e}${RESET}`);
+    }
+
+    console.log('');
+    return FAILED;
+};
+
+const run = async () => {
+    console.log(`Running ${Snapshot.tests.length} snapshot tests.`);
+
+    const fileToResult = {};
+    for (const test of Snapshot.tests) {
+        fileToResult[test] = await runSnapshotTest(test);
+    }
+
+    const getTestsByResult = r => Object.entries(fileToResult)
+        .filter(i => i[1] === r)
+        .map(i => i[0]);
+
+    const passed = getTestsByResult(PASSED);
+    const failed = getTestsByResult(FAILED);
+    const updated = getTestsByResult(UPDATED);
+
+    console.log('');
+    console.log(`${BOLD} === SUMMARY ===${RESET}`);
+    if (passed.length) {
+        console.log(`${BOLD}${GREEN}PASSED ${passed.length}${RESET}${GRAY} ${passed.join(', ')}${RESET}`);
+    }
+    if (failed.length) {
+        console.log(`${BOLD}${RED}FAILED ${failed.length}${RESET}${GRAY} ${failed.join(', ')}${RESET}`);
+    }
+    if (updated.length) {
+        console.log(`${BOLD}${BLUE}UPDATED ${updated.length}${RESET}${GRAY} ${updated.join(', ')}${RESET}`);
+    }
+
+    if (failed.length) {
+        console.log('');
+        console.log(`If the compiler's behavior has changed, this failure is expected.`);
+        console.log(`Update snapshots with ${BOLD}node snapshot-tests --update${RESET}`);
+        console.log(`Review the diff in version control, then commit the updated snapshot files.`);
+        process.exit(1);
+    }
+};
+
+run()
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
---
+++ test/integration/tw-snapshots.js
@@ -0,0 +1,20 @@
+const {test} = require('tap');
+const Snapshots = require('../../snapshot-tests/snapshots');
+
+for (const testName of Snapshots.tests) {
+    test(testName, async t => {
+        const expected = Snapshots.getExpectedSnapshot(testName);
+        if (expected) {
+            const actual = await Snapshots.generateActualSnapshot(testName);
+            if (actual === expected) {
+                t.pass('matches');
+            } else {
+                // This assertion will always fail, but tap will print a readable diff
+                t.equal(expected, actual, 'did not match; you may have to run: node snapshot-tests --update');
+            }
+        } else {
+            t.fail('missing snapshot, please run: node snapshot-tests --update');
+        }
+        t.end();
+    });
+}
